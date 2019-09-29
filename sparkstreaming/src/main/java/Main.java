57eb02406d77653afc5ae96c15c304eee84457af
iowTlo <iowTlo@qq.com>
2019-09-29 15:15:38 +0800

    添加了新的方法

--- sparkstreaming/src/main/java/Main.java
+++ sparkstreaming/src/main/java/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    public static void fun(){
+        System.out.println("这是新的方法");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("怎么可能还不进去");
